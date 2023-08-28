b0dbffc90e6cfa06e568de57f6f254026022aaf3
Ron Snyder <ron@snyderjr.com>
2023-08-27 22:14:54 -0400

    OSD with trigger

--- src/components/Image.ce.vue
+++ src/components/Image.ce.vue
@@ -10,51 +10,137 @@
 
   const root = ref<HTMLElement | null>(null)
   const osdEl = ref<HTMLElement | null>(null)
-  // const host = computed(() => (root.value?.getRootNode() as any)?.host)
-  // const shadowRoot = computed(() => root?.value?.parentNode as HTMLElement)
+  const host = computed(() => (root.value?.getRootNode() as any)?.host)
+  const shadowRoot = computed(() => root?.value?.parentNode as HTMLElement)
 
   const osdWidth = ref<number>()
   watch(osdWidth, () => { resize() })
 
   const imageSize = ref<ImageSize>()
   const aspectRatio = computed(() =>  Number(((imageSize.value?.width || 1)/(imageSize.value?.height || 1)).toFixed(4)) )
-  watch(aspectRatio, () => { resize() })
+  watch(aspectRatio, () => { 
+    resize()
+    if (osdEl.value) initOpenSeadragon()
+  })
 
   // TODO: debounce size updates
-  watch(osdEl, () => { 
-    if (osdEl.value)
-      new ResizeObserver(() => { osdWidth.value = osdEl.value?.clientWidth }).observe(osdEl.value)
+  watch(osdEl, () => {
+    if (!osdEl.value) return
+    if (props.width) {
+      root.value?.setAttribute('style', `width: ${props.width}px;margin: auto`)
+      osdWidth.value = props.width
+    } else {
+      new ResizeObserver(() => { osdWidth.value = osdEl.value?.clientWidth } ).observe(osdEl.value)
+    }
+    if (imageSize.value) initOpenSeadragon()
   })
 
   const props = defineProps({
     src: { type: String, required: true },
-    fit: { type: String, default: 'contain' }
+    width: { type: Number },
+    height: { type: Number },
+    left: { type: Boolean, default: false },
+    right: { type: Boolean, default: false },
+    cover: { type: Boolean, default: false },
+    fit: { type: String, default: 'contain' },
+    caption: { type: String },
+    noCaption: { type: Boolean, default: false }
   })
-  watch(props, () => { src.value = props.src })
+  watch(props, () => { evalProps() })
   
+  // OpenSeadragon - https://openseadragon.github.io/docs/
   const osd = ref<OpenSeadragon.Viewer>()
+
+  // OpenSeadragon tile source - https://openseadragon.github.io/docs/OpenSeadragon.TileSource.html
   const tileSource:any = ref<TiledImage>()
-  watch(tileSource, () => { osd.value?.open(tileSource.value) })
 
-  // watch(osd, () => { console.log(toRaw(osd.value)) })
-
-  const src = ref(props.src)
+  // Image source, can be an image or IIIF manifest URL
+  const src = ref()
   watch(src, () => { getTileSource(src.value) })
+  function setSrc(_src: string) {
+    src.value = /^\w+:/.test(_src) 
+      ? `https://iiif.juncture-digital.org/${_src.replace(/ /g, '_')}/manifest.json` 
+      : _src
+  }
 
-  onMounted(() => { 
-    osd.value = initOpenSeadragon()
-    getTileSource(src.value)
+  const manifest = ref()
+  const caption = computed(() => props.caption || (manifest.value && manifest.value?.label?.en?.[0]))
+  // watch(manifest, () => { console.log('manifest', toRaw(manifest.value)) })
+
+  const coords = ref<string>()
+
+  function evalProps() {
+    setSrc(props.src)
+  }
+
+  onMounted(() => {
+    evalProps()
+    addInteractionHandlers()
   })
+
+  // detect if src is a IIIF manifest
+  async function isIIIFfManifest(src: string) {
+    if (src.indexOf('manifest.json') > 0) return true
+    let resp = await fetch(src, {method: 'HEAD'})
+    return resp.headers.get('Content-Type')?.indexOf('image') === -1
+  }
+
+  // convert IIIF v2 manifest to v3; all operations in this component are based on v3
+  async function prezi2to3(manifest: any) {
+    let resp = await fetch('https://iiif.juncture-digital.org/prezi2to3/', {
+      method: 'POST', 
+      body: JSON.stringify(manifest)
+    })
+    if (resp.ok) return (await resp).json()
+  }
 
   async function getTileSource(src:string) {
-    getImageSize(src).then((size) => { imageSize.value = size })
-    tileSource.value = { url: src, type: 'image', buildPyramid: true }
+    if (await isIIIFfManifest(src)) {
+      let manifest = await getManifest(src)
+      let itemInfo = findItem({type:'Annotation', motivation:'painting'}, manifest).body
+      imageSize.value = { width: itemInfo.width, height: itemInfo.height}
+      tileSource.value = itemInfo.service
+        ? `${(itemInfo.service[0].id || itemInfo.service[0]['@id'])}/info.json`
+        : { url: itemInfo.id, type: 'image', buildPyramid: true }
+    } else {
+      getImageSize(src).then((size) => { imageSize.value = size })
+      tileSource.value = { url: src, type: 'image', buildPyramid: true }
+    }
   }
 
+  // find an item in a IIIF manifest
+  function findItem(toMatch: object, current: object, seq: number = 1): any {
+    const found = _findItems(toMatch, current)
+    return found.length >= seq ? found[seq-1] : null
+  }
+
+  // recursive helper for finding items in a IIIF manifest
+  function _findItems(toMatch: object, current: any, found: object[] = []) {
+    found = found || []
+    if (current.items) {
+      for (let i = 0; i < current.items.length; i++ ) {
+        let item = current.items[i]
+        let isMatch = !Object.entries(toMatch).find(([attr, val]) => item[attr] && item[attr] !== val)
+        if (isMatch) found.push(item)
+        else _findItems(toMatch, item, found)
+      }
+    }
+    return found
+  }
+
+  // get a IIIF manifest, convert to v3 if necessary
   async function getManifest(src:any) {
-    console.log(`getManifest(${src})`)
+    let resp = await fetch(src)
+    if (resp.ok) {
+      let _manifest:any = await resp.json()
+      manifest.value = parseFloat(_manifest['@context'].split('/').slice(-2,-1).pop()) < 3
+        ? await prezi2to3(manifest)
+        : _manifest
+      return manifest.value
+    }
   }
 
+  // get image size
   async function getImageSize(src: string): Promise<ImageSize> {
     return new Promise((resolve, reject) => {
       let img = new Image()
@@ -64,51 +150,173 @@
     })
   }
 
+  function setOsdHeight() {
+    if (osdEl.value?.clientWidth) {
+      let osdHeight = props.height || Number(osdEl.value?.clientWidth / aspectRatio.value).toFixed(0)
+      // console.log(`setOsdHeight() width:=${osdWidth.value} height=${osdHeight}`)
+      osdEl.value?.setAttribute('style', `height: ${osdHeight}px;`)
+    }
+  }
+
+  // resize OSD viewer
   function resize() {
-    if (osdWidth.value === undefined) return
-    let osdHeight = osdWidth.value / aspectRatio.value
-    // console.log(`resize() width: ${osdWidth.value} height: ${osdHeight}`)
-    osdEl.value?.setAttribute('style', `height: ${osdHeight}px;`)
-    setTimeout(() => osd.value?.viewport?.goHome(true), 10)
+    setOsdHeight
+    setTimeout(() => osd.value?.viewport?.goHome(true), 100)
   }
 
   function initOpenSeadragon() {
-    let shadowRoot: any = root.value?.parentNode
-    let container = shadowRoot.querySelector('#osd')
+    if (!osdEl.value) return
+    let container = shadowRoot.value.querySelector('#osd') as HTMLElement
+    // console.log(`initOpenSeadragon() width: ${osdEl.value?.clientWidth} imageHeight: ${imageSize.value?.height} osdHeight: ${osdEl.value?.clientHeight} tileSource: ${tileSource.value}`)
+    setOsdHeight()
     const osdOptions: OpenSeadragon.Options = {
       element: container,
       prefixUrl: 'https://openseadragon.github.io/openseadragon/images/',
-      homeFillsViewer: props.fit === 'cover',
-      showNavigationControl: true,
-      minZoomImageRatio: 1,
+      tileSources: tileSource.value,
+      homeFillsViewer: props.cover || props.fit === 'cover',
+      // showNavigationControl: true,
+      // minZoomImageRatio: 1,
       maxZoomPixelRatio: 10,
-      showRotationControl: true,
-      showHomeControl: true,
-      showZoomControl: true,
-      showFullPageControl: true,
-      showNavigator: false,
-      sequenceMode: true,
-      showReferenceStrip: true,
+      // showRotationControl: true,
+      // showHomeControl: true,
+      // showZoomControl: true,
+      // showFullPageControl: true,
+      // showNavigator: false,
+      // sequenceMode: true,
+      // showReferenceStrip: true,
       
-      animationTime: 0.5,
-      springStiffness: 10,
+      // animationTime: 0.5,
+      // springStiffness: 10,
       
-      visibilityRatio: 1.0,
-      constrainDuringPan: true
+      // visibilityRatio: 1.0,
+      // constrainDuringPan: true
       
     }
-    return OpenSeadragon(osdOptions)
+    osd.value = OpenSeadragon(osdOptions)
+    osd.value.addHandler('viewport-change', () => watchCoords())
+    setTimeout(() => setViewportCoords(), 500)
   }
+
+  const actionKeys = new Set(['zoomto'])
+  function addInteractionHandlers() {
+    let el = host.value.parentElement
+    while (el.parentElement && el.tagName !== 'BODY') el = el.parentElement;
+    (Array.from(el.querySelectorAll('ez-trigger')) as HTMLElement[]).forEach(mark => {
+      let match = Array.from(mark.attributes).find(attr => actionKeys.has(attr.name))
+      if (match) {
+        let imageEl = findImageEl(mark)
+        if (imageEl) {
+          mark.classList.add(match.name)
+          mark.addEventListener('click', () => {
+            if (match?.name === 'zoomto') zoomto(match.value)
+          })
+        }
+      }
+    })
+  }
+
+  function findImageEl(el:any) {
+
+    function checkSibs(el:any) {
+      let sib = el.previousSibling
+      while (sib) {
+        if (sib.nodeName === 'EZ-IMAGE') return sib === host.value ? sib : null
+        sib = sib.previousSibling
+      }
+    }
+  
+    while (el.parentElement && el.tagName !== 'BODY') {
+      checkSibs(el)
+      el = el.parentElement
+      let imageEl = el.querySelector(':scope > ez-image, :scope > p > ez-image')
+      if (imageEl) return imageEl === host.value ? imageEl : null
+    }
+  }
+
+  let zoomedToRegion:string = ''
+  function zoomto(arg: string) {
+    arg = arg.replace(/^zoomto\|/i,'')
+    const match = arg?.match(/^(?<region>(pct:|pixel:|px:)?[\d.]+,[\d.]+,[\d.]+,[\d.]+)?$/)
+    if (match) {
+      let region = match?.groups?.region
+      // console.log(`ez-image.zoomto: region=${region}`)
+      if (region) {
+        if (zoomedToRegion === region) {
+          osd.value?.viewport.goHome()
+          zoomedToRegion = ''
+        } else {
+          zoomedToRegion = region
+          osd.value?.viewport.fitBounds(parseRegionString(region, osd.value), false)
+        }
+      }
+    }
+  }
+
+  function parseRegionString(region: string, viewer: OpenSeadragon.Viewer) {
+  let viewportRect
+  const s1 = region.split(':')
+  let [x,y,w,h] = s1[s1.length-1].split(',').map(v => parseInt(v))
+  const size = viewer.world.getItemAt(0).getContentSize()
+  if (s1.length === 2 && (s1[0] === 'pct' || s1[0] === 'percent')) {
+    x = Math.round(size.x * x/100),
+    y = Math.round(size.y * y/100),
+    w = Math.round(size.x * w/100), 
+    h = Math.round(size.y * h/100)
+  }
+  // viewportRect = viewer.viewport.imageToViewportRectangle(rect)
+  viewportRect = viewer.viewport.imageToViewportRectangle(x,y,w,h)
+  return viewportRect
+}
+
+function setViewportCoords() {
+  const tiledImage = osd.value?.world.getItemAt(0)
+  if (tiledImage) {
+    let bounds = osd.value?.viewport.getBounds()
+    const imageBounds = bounds ? tiledImage.viewportToImageRectangle(bounds) : null
+    coords.value = imageBounds
+      ? `${Math.ceil(imageBounds.x)},${Math.ceil(imageBounds.y)},${Math.ceil(imageBounds.width)},${Math.ceil(imageBounds.height)}`
+      : ''
+  }
+}
+
+let coordsDebounce: any = null
+  function watchCoords() {
+    if (coordsDebounce !== null) {
+      clearTimeout(coordsDebounce)
+      coordsDebounce = null
+    }
+    coordsDebounce = window.setTimeout(() => setViewportCoords(), 100)
+  }
+
+function copyTextToClipboard(text: string) {
+  if (navigator.clipboard) navigator.clipboard.writeText(text)
+}
 
 </script>
 
 <template>
 
-<div ref="root" class="h-auto w-1/2 float-right rounded overflow-hidden shadow-lg ml-4">
+<div ref="root" 
+  class="rounded overflow-hidden shadow-lg ml-4"
+  :class="{ 
+    'float-left': left ,
+    'float-right': right,
+    'w-1/2': left || right,
+    'w-full': !left && !right,
+  }"
+>
   <!-- <img class="w-full" :src="src" alt="Image title"> -->
-  <div ref="osdEl" id="osd" class="w-full h-96"></div>
-  <div class="px-6 py-4">
-    <div class="font-bold text-xl mb-2">Image title</div>
+  <div v-if="tileSource" ref="osdEl" id="osd" class="w-full" role="img" :aria-label="caption" :alt="caption"></div>
+  <div v-if="tileSource && caption && !noCaption" class="group relative h-10 p-2">
+    <div class="font-bold text-xl mb-2" v-html="caption"></div>
+    <div v-if="coords"
+      class="invisible group-hover:visible 
+            absolute pr-4 pl-4 pt-2 pb-2 mt-24 
+            bottom-0 right-0 cursor-copy
+            ml-4 bg-slate-100 text-black"
+      v-html="coords" 
+      @click="copyTextToClipboard(coords || '')">
+    </div>
   </div>
 </div>
 
