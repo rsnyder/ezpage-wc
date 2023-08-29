7f1929441f6ac9834c9106565e5af2609a472b4d
Ron Snyder <ron@snyderjr.com>
2023-08-29 15:35:36 -0400

    testing

--- src/utils.ts
+++ src/utils.ts
@@ -286,7 +286,7 @@ export async function getHtml() {
 export async function convertToEzElements() {
 
   let isGhp = isGHP()
-  let config = await getConfig()
+  let config = (window as any).site
   console.log(`isGhp=${isGhp} origin=${location.origin}`, config)
   
   document.querySelectorAll('a').forEach(link => {
